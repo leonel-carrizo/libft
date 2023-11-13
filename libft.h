205df9f962ae3faff9c0742c33dc63169cfd0f72
Leonel Ricardo Carrizo Marin <lcarrizo@c3r5s1.42london.com>
2023-11-13 19:10:23 +0000

    add fisrt prototypes to libft.h

--- libft.h
+++ libft.h
@@ -6,13 +6,34 @@
 /*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 21:08:46 by lcarrizo          #+#    #+#             */
-/*   Updated: 2023/11/05 21:25:33 by lcarrizo         ###   ###.london.com    */
+/*   Updated: 2023/11/13 18:53:45 by lcarrizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-size_t		ft_strlen(char *s);
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stddef.h>
+
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+char		*ft_strrchr(const char *s, int c);
+size_t		ft_strlen(const char *s);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
