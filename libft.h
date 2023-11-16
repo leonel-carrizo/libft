5cb7660b677972bf4949a82debd638a46101fd04
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2023-11-16 18:59:53 +0000

    add libft.h with atoi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 21:08:46 by lcarrizo          #+#    #+#             */
-/*   Updated: 2023/11/14 17:50:28 by lcarrizo         ###   ###.london.com    */
+/*   Updated: 2023/11/16 15:49:29 by lcarrizo         ###   ###.london.com    */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
+int			atoi(const char *nptr);
 
 #endif
