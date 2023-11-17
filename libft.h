54c8e51f5c753a849e63c322c80514cc0888ae4b
Leonel Ricardo Carrizo Marin <lcarrizo@c3r5s1.42london.com>
2023-11-17 15:42:39 +0000

    add prototype ft_strjoin function to libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 21:08:46 by lcarrizo          #+#    #+#             */
-/*   Updated: 2023/11/17 12:10:08 by lcarrizo         ###   ########.fr       */
+/*   Updated: 2023/11/17 14:05:04 by lcarrizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ int			atoi(const char *nptr);
 void		*ft_calloc(size_t nmemb, size_t size);
 char		*ft_strdup(const char *s);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
 
 #endif
