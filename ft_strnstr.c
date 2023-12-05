97503fe34569427714ce8c5a554fd0d29c5979a4
Leonel Ricardo Carrizo Marin <lcarrizo@c3r5s1.42london.com>
2023-12-05 06:11:39 +0000

    redo ft_strnstr function for secound evaluation

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,81 +6,47 @@
 /*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 19:11:09 by lcarrizo          #+#    #+#             */
-/*   Updated: 2023/12/01 17:10:00 by lcarrizo         ###   ########.fr       */
+/*   Updated: 2023/12/05 05:58:31 by lcarrizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*check_substr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(char const *str, char const *to_find, size_t len)
 {
-	char	*result;
-	char	*slittle;
-	int		sub_len;
+	size_t	i;
 
-	while (*big)
+	if (!str && !len)
+		return (NULL);
+	if (!*to_find)
+		return ((char *)str);
+	while (*str && len--)
 	{
-		while (*big && *big != *little && len--)
-			big++;
-		result = (char *)big;
-		slittle = (char *)little;
-		sub_len = len;
-		while (*slittle == *result && sub_len)
+		i = 0;
+		while (str[i] == to_find[i] && to_find[i] && i <= len)
 		{
-			result++;
-			slittle++;
-			sub_len--;
+			if (!to_find[i + 1])
+				return ((char *)str);
+			i++;
 		}
-		if (*slittle == '\0')
-			return ((char *)big);
-		big++;
-		len--;
+		str++;
 	}
-	return (NULL);
-}
-
-char	*ft_strnstr(const char *big, const char *little, size_t len)
-{
-	size_t	llen;
-	char	*ptr;
-
-	if (!big && !len)
-		return (NULL);
-	llen = ft_strlen(little);
-	if (little == NULL || llen <= 0)
-		return ((char *)big);
-	if (llen > len)
-		return (NULL);
-	ptr = check_substr(big, little, len);
-	if (ptr)
-		return (ptr);
 	return (NULL);
 }
 
 //int	main(void)
 //{
-//	//const char large[] = "Foo Bar Baz";
-//	//const char small[] = "Baz";
+////	const char *large = "MZIRIBMZIRIBMZE123";
+////	const char *small = "MZIRIBMZE";
 //	//char	*ptr;
 ////	char *empty = (char*)"";
 //	char haystack[30] = "aaabcabcd";
 ////	char needle[10] = "aabc";
 //
 //	//ptr = ft_strnstr(large, small, 11);
+//	//printf("This is FT: %s\n", ft_strnstr(large, small, ft_strlen(small)));
+//	//printf("This is C: %s\n", strnstr(large, small, ft_strlen(small)));
 //	printf("This is FT: %s\n", ft_strnstr(haystack, "cd", 8));
 //	printf("This is C: %s\n", strnstr(haystack, "cd", 8));
-//	//printf("This is C: %s\n", strnstr(large, small, 11));
 //	return (0);
 //}
-
-//DESCRIPTION
-//The  strnstr()  function  locates the first occurrence of the null-termi‐
-//nated string little in the string big, where not more than len characters
-//are searched.  Characters that appear after  a  ‘\0’  character  are  not
-//searched.   Since  the  strnstr()  function is a FreeBSD specific API, it
-//should only be used when portability is not a concern.
-//
-//RETURN VALUES
-//If little is an empty string, big is returned; if little  occurs  nowhere
-//in  big,  NULL is returned; otherwise a pointer to the first character of
-//the first occurrence of little is returned.
